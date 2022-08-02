d039afe2b3d7c6b5534bb0cd68a831e627794d7e
andrade-fernanda <fvw.andrade@gmail.com>
2022-08-01 22:00:05 -0300

    Primeiro commit

---
+++ blogPessoal/src/main/java/com/generation/blogPessoal/repository/PostagemRepository.java
@@ -0,0 +1,15 @@
+package com.generation.blogPessoal.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.generation.blogPessoal.model.PostagemModel;
+
+@Repository
+public interface PostagemRepository extends JpaRepository<PostagemModel, Long>{
+	public List <PostagemModel> findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo);
+
+}
